e03d4f9fd35f1bafb7b74fb108af95faea8ca2dc
RINTAROOKAMOTO <56873863+RINTAROOKAMOTO@users.noreply.github.com>
2019-11-08 18:56:51 +0900

    zzz

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'comments#index'
+  resources :users, only: [:index, :create, :destroy, :edit, :update]
   resources :comments, only: :index
 end
