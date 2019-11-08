950240a5952e26577d2c81b068a7047bd640be94
RINTAROOKAMOTO <56873863+RINTAROOKAMOTO@users.noreply.github.com>
2019-11-08 19:36:42 +0900

    Revert "zzz"
    This reverts commit e03d4f9fd35f1bafb7b74fb108af95faea8ca2dc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'comments#index'
-  resources :users, only: [:index, :create, :destroy, :edit, :update]
   resources :comments, only: :index
 end
